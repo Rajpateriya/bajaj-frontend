5676fc8d62514581cd17f6055258b043386f8189
Rajpateriya <pateriyaraj52@gmail.com>
2024-11-22 17:25:01 +0530

    completed frontend

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import JsonProcessor from './components/JsonProcessor.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <main className="App-main">
+        <JsonProcessor />
+      </main>
+    </div>
+  );
+}
+
+export default App;
